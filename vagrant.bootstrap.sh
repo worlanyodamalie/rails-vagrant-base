1601c484aedbee0ab46eb0b81903b67f6a1cb12d
Worlanyo <worlalidamalie@gmail.com>
2017-04-05 13:27:39 +0000

    Update vagrant.bootstrap.sh

--- vagrant.bootstrap.sh
+++ vagrant.bootstrap.sh
@@ -1,26 +1,21 @@
 #!/usr/bin/env bash
-
-# Configuration:
-#   Which versions of Ruby and Rails do you want to install?
 RUBY=2.3.3
 RAILS=5.0.2
-# /Configuration
-
 sudo apt-get update
 
-echo "==> Installing git..."
+echo "==> Installing git...";
 sudo apt-get install -y git
 
-echo "==> Installing nodejs..."
+echo "==> Installing nodejs...";
 sudo apt-get install -y nodejs
 
-echo "==> Installing Ruby dependencies..."
+echo "==> Installing Ruby dependencies...";
 sudo apt-get install -y build-essential bison openssl libreadline6 libreadline6-dev zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf libc6-dev ssl-cert subversion ruby-dev
 
-echo "==> Installing Postgresql"
+echo "==> Installing Postgresql";
 sudo apt-get install -y postgresql-9.3 postgresql-server-dev-9.3 postgresql-contrib-9.3
 
-echo "==> Installing rbenv and plugins..."
+echo "==> Installing rbenv and plugins...";
 sudo apt-get install -y rbenv
 if [ ! -d ~/.rbenv/plugins/ruby-build ]; then
 	git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
@@ -35,9 +30,9 @@ source ~/.bashrc
 mkdir -p ~/.rbenv
 sudo chown -R ubuntu ~/.rbenv
 if [ -f ~/.rbenv/versions/$RUBY/bin/ruby ]; then
-	echo "==> Ruby ${RUBY} is already installed..."
+	echo "==> Ruby ${RUBY} is already installed...";
 else
-	echo "==> Installing Ruby ${RUBY}..."
+	echo "==> Installing Ruby ${RUBY}...";
 	rbenv install $RUBY
 fi
 rbenv global $RUBY
